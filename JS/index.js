f931338001c7ef6d930bc358c43327de84e156fe
Joscelyn <JoscelynRose@gmail.com>
2020-01-23 20:24:47 -0500

    finished the header links

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,20 @@
 let navWrapper = document.querySelector(".nav-wrapper");
 let navToogler = document.querySelector(".nav-toogler");
-let seeMyWork = document.querySelector(".portfolio-btn");
-let mybutton = document.getElementById("myBtn");
-console.log(seeMyWork, "seeMyWork");
+
+// let myBtn = document.getElementById("myBtn");
+// myBtn.href = "#section-portfolio";
+
+// let body = document.body,
+//   html = document.documentElement;
+
+// let height = Math.max(
+//   body.scrollHeight,
+//   body.offsetHeight,
+//   html.clientHeight,
+//   html.scrollHeight,
+//   html.offsetHeight
+// );
+
 navToogler.addEventListener("click", function(event) {
   navWrapper.classList.toggle("active");
 });
-
-function topFunction() {
-  document.body.scrollBy(0, 1650); // For Safari
-  document.documentElement.scrollBy(0, 1650);
-
-  // look into "offsetTop" to set scrollBy to that
-  // window.scrollBy(100, 0);// For Chrome, Firefox, IE and Opera
-}
