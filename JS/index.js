2d9782ca11fffdad61c9866a4347186a2ec90dfa
Joscelyn <JoscelynRose@gmail.com>
2020-01-25 15:48:26 -0500

    WIP: fixing contact form

--- JS/index.js
+++ JS/index.js
@@ -18,3 +18,26 @@ let navToogler = document.querySelector(".nav-toogler");
 navToogler.addEventListener("click", function(event) {
   navWrapper.classList.toggle("active");
 });
+
+const processForm = form => {
+  const data = new FormData(form);
+  data.append("form-name", "contact");
+  fetch("/", {
+    method: "POST",
+    body: data
+  })
+    .then(() => {
+      form.innerHTML = `<div class="form--success">Almost there! Check your inbox for a confirmation e-mail.</div>`;
+    })
+    .catch(error => {
+      form.innerHTML = `<div class="form--error">Error: ${error}</div>`;
+    });
+};
+
+const emailForm = document.querySelector(".email-form");
+if (emailForm) {
+  emailForm.addEventListener("submit", e => {
+    e.preventDefault();
+    processForm(emailForm);
+  });
+}
