774145518edc84207bd3da0007c79694455ea281
Joscelyn <JoscelynRose@gmail.com>
2020-01-26 23:30:58 -0500

    changed portfolio section to use components

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,6 @@
 let navWrapper = document.querySelector(".nav-wrapper");
 let navToogler = document.querySelector(".nav-toogler");
 
-// let myBtn = document.getElementById("myBtn");
-// myBtn.href = "#section-portfolio";
-
-// let body = document.body,
-//   html = document.documentElement;
-
-// let height = Math.max(
-//   body.scrollHeight,
-//   body.offsetHeight,
-//   html.clientHeight,
-//   html.scrollHeight,
-//   html.offsetHeight
-// );
-
 navToogler.addEventListener("click", function(event) {
   navWrapper.classList.toggle("active");
 });
